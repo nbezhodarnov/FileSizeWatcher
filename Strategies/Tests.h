8b7cdd7bd61f9db9b42f72e5bd34329f86afe5a7
Nblaaa <nblaaa@mail.ru>
2020-05-13 00:26:15 +0700

    Tests adopted to both Linux and Windows

--- Strategies/Tests.h
+++ Strategies/Tests.h
@@ -25,6 +25,14 @@ public:
         test_results_expected[3][1] = QString::fromUtf8("The folder is empty.\n");
         test_results_expected[4][1] = QString::fromUtf8(".txt, size percentage: 100%\n");
         test_results_expected[5][1] = QString::fromUtf8("The object doesn\'t exist.\n");
+        #if defined(Q_OS_LINUX)
+        test_results_expected[0][0] = QString::fromUtf8("Heroes-II-of-Might-and-Magic-2-icon.png, size percentage: 95.2159%\nТекстовый файл (1).txt, size percentage: 0.0346504%\nТекстовый файл.txt, size percentage: 0.0184802%\n");
+        test_results_expected[1][0] = QString::fromUtf8("Новая папка, size percentage: 3.41547%\nНовая папка (1), size percentage: 24.3902%\nHeroes-II-of-Might-and-Magic-2-icon.png, size percentage: 68.7405%\nТекстовый файл (1).txt, size percentage: 0.0250156%\nТекстовый файл.txt, size percentage: 0.0133417%\n");
+        test_results_expected[2][0] = QString::fromUtf8(".vs, size percentage: 99.8418%\nTests, size percentage: 0%\nНовая папка, size percentage: 0.00513362%\nНовая папка (1), size percentage: 0.0390762%\nНовая папка (2), size percentage: 0.00530532%\nHeroes-II-of-Might-and-Magic-2-icon.png, size percentage: 0.10332%\nTests.lnk, size percentage: 0.000171705%\nТекстовый файл (1).txt, size percentage: 3.75997e-05%\nТекстовый файл.txt, size percentage: 2.00532e-05%\n");
+        test_results_expected[0][1] = QString::fromUtf8(".png, size percentage: 95.2159%\n.txt, size percentage: 0.0531307%\ndirectory, size percentage: 4.73094%\n");
+        test_results_expected[1][1] = QString::fromUtf8(".png, size percentage: 68.7405%\n.sav, size percentage: 17.5043%\n.txt, size percentage: 0.0933917%\ndirectory, size percentage: 13.6619%\n");
+        test_results_expected[2][1] = QString::fromUtf8(".db, size percentage: 15.4881%\n.ipch, size percentage: 84.2735%\n.lnk, size percentage: 0.000343411%\n.png, size percentage: 0.10332%\n.sav, size percentage: 0.0287262%\n.suo, size percentage: 0.0442775%\n.txt, size percentage: 0.000140372%\ndirectory, size percentage: 0.0616034%\n");
+        #endif
         QString path = QString::fromUtf8("../FileSizeWatcher_Part1/Tests/Test0");
         for (int i = 1; i < 7; i++) {
             switch (i) {

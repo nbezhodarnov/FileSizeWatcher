8ce0351de4aff4ba7a0c379154942ec5890a3c44
Nblaaa <nblaaa@mail.ru>
2020-05-31 00:23:20 +0700

    Added QAbstractItemView, slot on_selectionChangedSlot.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#include <QAbstractItemView>
-#include <QFileSystemModel>
+#include <QItemSelectionModel>
 #include <QTableView>
 #include <QListView>
 #include <QTreeView>
@@ -13,7 +12,6 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     QString homePath = QDir::homePath();
-    QFileSystemModel *dirModel, *fileModel;
     // Определим  файловой системы:
     dirModel = new QFileSystemModel(this);
     dirModel->setFilter(QDir::NoDotAndDotDot | QDir::AllDirs);
@@ -21,7 +19,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->folderTreeView->setModel(dirModel);
     ui->folderTreeView->expandAll();
 
-    QAbstractItemView *view = new QListView(this);
+    view = new QListView(this);
     ui->splitter->addWidget(view);
     fileModel = new QFileSystemModel(this);
     fileModel->setFilter(QDir::NoDotAndDotDot | QDir::Files);
@@ -31,6 +29,56 @@ MainWindow::MainWindow(QWidget *parent) :
     view = new QTableView();
     view->setModel(fileModel);
     ui->splitter->addWidget(view);
+
+    QItemSelectionModel *selectionModel = ui->folderTreeView->selectionModel();
+
+    QModelIndex indexHomePath = dirModel->index(homePath);
+    QFileInfo fileInfo = dirModel->fileInfo(indexHomePath);
+
+    ui->folderTreeView->header()->resizeSection(0, 200);
+    //Выполняем соединения слота и сигнала который вызывается когда осуществляется выбор элемента в TreeView
+    connect(selectionModel, SIGNAL(selectionChanged(const QItemSelection &, const QItemSelection &)), this, SLOT(on_selectionChangedSlot(const QItemSelection &, const QItemSelection &)));
+    //Пример организации установки курсора в TreeView относительно модельного индекса
+    QItemSelection toggleSelection;
+    QModelIndex topLeft;
+    topLeft = dirModel->index(homePath);
+    dirModel->setRootPath(homePath);
+
+    toggleSelection.select(topLeft, topLeft);
+    selectionModel->select(toggleSelection, QItemSelectionModel::Toggle);
+}
+
+void MainWindow::on_selectionChangedSlot(const QItemSelection &selected, const QItemSelection &deselected)
+{
+    //Q_UNUSED(selected);
+    Q_UNUSED(deselected);
+    QModelIndex index = ui->folderTreeView->selectionModel()->currentIndex();
+    QModelIndexList indexs = selected.indexes();
+    QString filePath = "";
+
+    // Размещаем инфо в statusbar относительно выделенного модельного индекса
+
+    if (indexs.count() >= 1) {
+        QModelIndex ix =  indexs.constFirst();
+        filePath = dirModel->filePath(ix);
+        this->statusBar()->showMessage("Выбранный путь: " + dirModel->filePath(indexs.constFirst()));
+    }
+
+    //TODO: !!!!!
+    /*
+    Тут простейшая обработка ширины первого столбца относительно длины названия папки.
+    Это для удобства, что бы при выборе папки имя полностью отображалась в  первом столбце.
+    Требуется доработка(переработка).
+    */
+    int length = 200;
+    int dx = 30;
+
+    if (dirModel->fileName(index).length() * dx > length) {
+        length = length + dirModel->fileName(index).length() * dx;
+    }
+
+    ui->folderTreeView->header()->resizeSection(index.column(), length + dirModel->fileName(index).length());
+    view->setRootIndex(fileModel->setRootPath(filePath));
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,13 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QAbstractItemView>
+#include <QFileSystemModel>
+#include <QItemSelection>
 #include <QMainWindow>
+
+#include "Strategies/filetypestrategy.h"
+#include "Strategies/folderstrategy.h"
 
 namespace Ui {
 class MainWindow;
@@ -11,13 +17,18 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+private slots:
+    void on_selectionChangedSlot(const QItemSelection &selected, const QItemSelection &deselected);
+
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
-    QWidget *view;
+    QAbstractItemView *view;
+    ExplorerStrategy *groupingStrategy;
+    QFileSystemModel *dirModel, *fileModel;
 };
 
 #endif // MAINWINDOW_H
